1b78fd1ccb3c23c96f9f59f03e33337718c01e27
Waldo Heudens <waldo@heudens.be>
2021-10-06 12:26:50 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// Demonstratie van het gebruik van Tuple
+
+using GebruikVanTuple;
+
+Student student = new Student()
+                        { 
+                            Id = 20210197,
+                            Ingeschreven = new DateTime(2021, 9, 15),
+                            GeboorteDatum= new DateTime(2003, 3, 18),
+                            Naam = "Jan Jansens",
+                            Postcode= "1070",
+                            Richting = "Graduaat Programmeren",
+                            Straat = "Onderwijsstraat",
+                            Woonplaats = "Anderlecht"
+                        };
+
+// Ophalen inschrijvingsgegevens zonder Tuple:
+Inschrijvingsgegevens ig = student.Inschrijving();
+Console.WriteLine("Inschrijving: Student " + ig.Id + " is ingeschreven op " + ig.Ingeschreven + " voor " + ig.Richting);
+
+// Ophalen inschrijvingsgegevens met Tuple:
+Tuple<int, DateTime, string> inschrijving = student.InschrijvingMetTuple();
+Console.WriteLine("Inschrijving: Student " + inschrijving.Item1 + " is ingeschreven op " + inschrijving.Item2 + " voor " + inschrijving.Item3);
---
+++ Student.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GebruikVanTuple
+{
+    internal class Student
+    {
+        internal int Id {  get; set; }
+        internal string Naam {  get; set; }
+        internal DateTime GeboorteDatum {  get; set; }
+        internal DateTime Ingeschreven {  get; set; }
+        internal string Richting {  get; set; }
+        internal string Straat {  get; set; }
+        internal string Postcode {  get; set; }
+        internal string Woonplaats {  get; set; }
+
+        // ...
+        // Voor de student zijn constructors en andere methoden aanwezig
+        // ...
+
+        // We voorzien een methode "Inschrijving" die slechts 3 waarden teruggeeft:
+        //      Id, Ingeschreven en Richting
+
+        // Traditioneel moeten we daarvoor een class of een struct voorzien, en wordt dit als volgt uitgewerkt:
+        internal Inschrijvingsgegevens Inschrijving()
+        {
+            return new Inschrijvingsgegevens()
+                        {
+                            Id = Id,
+                            Ingeschreven = Ingeschreven,
+                            Richting = Richting
+                        };
+        }
+
+        // Je kan deze methode ook schrijven zonder een struct (of klasse) Inschrijvingsgegevens te maken:
+        internal Tuple<int, DateTime, string> InschrijvingMetTuple()
+        {
+            return Tuple.Create(Id, Ingeschreven, Richting);
+        }
+
+    }
+
+    internal struct Inschrijvingsgegevens
+    {
+        internal int Id;
+        internal DateTime Ingeschreven;
+        internal string Richting;
+    }
+}
